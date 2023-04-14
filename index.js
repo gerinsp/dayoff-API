2e6bb45ca9ccd6a59e74d29987e5f1e7e8554ced
gerinsp <denapratama7@gmail.com>
2023-04-14 11:28:21 +0700

    fix bug params

--- index.js
+++ index.js
@@ -62,6 +62,48 @@ app.get('/api', (req, res) => {
             }
           })
         })
+      } else {
+        $('article ul').each((i, ul) => {
+            const $ul = $(ul);
+            const Tahun = $ul.find('li b').eq(0).text();
+            const strBulan = $ul.find('li a').eq(0).text();
+            const cleanedStr = strBulan.replace(/\d+/g, '');
+    
+            const mapBulan = {
+              'januari': '01',
+              'februari': '02',
+              'maret': '03',
+              'april': '04',
+              'mei': '05',
+              'juni': '06',
+              'juli': '07',
+              'agustus': '08',
+              'september': '09',
+              'oktober': '10',
+              'november': '11',
+              'desember': '12'
+            }
+    
+            const Bulan = cleanedStr.toLowerCase().trim();
+            const kodeBulan = mapBulan[Bulan];
+    
+            $ul.find('li').eq(3).find('tbody tr').each((i, tr) => {
+              const $tr = $(tr);
+              const namaHariLibur = $tr.find('td').eq(1).text();
+              const tanggal = $tr.find('td').eq(0).text();
+              if (tanggal.includes('-')) {
+                const date = tanggal.split('-');
+                const start = parseInt(date[0]);
+                const end = parseInt(date[1]);
+    
+                for (let i = start; i <= end; i++) {
+                  data.push({ tanggal: Tahun + '-' + kodeBulan + '-' + i.toString(), keterangan: namaHariLibur })
+                }
+              } else {
+                data.push({ tanggal: Tahun + '-' + kodeBulan + '-' + tanggal, keterangan: namaHariLibur });
+              }
+            })
+          })
       }
       res.json(data) // Mengirim file HTML sebagai response
     } else {
